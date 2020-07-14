ae46c7a7e5ff2804e85bbeed0f55397fe2348ece
johnduva <johnduva83@gmail.com>
2020-07-14 14:42:17 -0400

    Jul 14 1442

--- John Analysis/July/velVsLagPlot.m
+++ John Analysis/July/velVsLagPlot.m
@@ -11,30 +11,81 @@ paws = [ zscore(squeeze(allPaws(2,2,:))), zscore(squeeze(allPaws(2,3,:))) ];
 
 % Get the minpeaks so we can consider each 2π phase
 for k = 1:2
-    [max, maxIndex] = findpeaks(paws(:,k),'MinPeakDistance', 9);
-    maxpkx{k} = [maxIndex, max];
-    clear maxpksIndex maxpks
-    
-    [min, minIndex] = findpeaks(-paws(:,k),'MinPeakDistance', 9);
-    minpkx{k} = [minIndex, -min];
-    clear minpksIndex minpks 
+    [maxx, maxIndex] = findpeaks(paws(:,k),'MinPeakDistance', 9);
+    maxpkx{k} = [maxIndex, maxx];
+    clear maxx maxIndex
+%     [minn, minIndex] = findpeaks(-paws(:,k),'MinPeakDistance', 9);
+%     minpkx{k} = [minIndex, -minn];
+%     clear minn minIndex
 end
+
+%% Get the cross correlation of the y-values from maxpk(k) to maxpk(k+1)
+
+% Get the centroid locations from 'keepersOnly' (only 27,525 instead of 95k)
+index = 1;
+for frame = 1: length(allTracks{pORa{phe}(1,an),day})
+    if ismember(frame, keepersOnly{pORa{phe}(1,an),day}) % && frame == blank + 1
+        centroidsF2(index,1) = allTracks{pORa{phe}(1,an),day}(frame,1); 
+        centroidsF2(index,2) = allTracks{pORa{phe}(1,an),day}(frame,2);
+        index = index + 1;
+    end
+end
+
+% Convert the centroid locations to velocities
+% Get change in x-coordinate per frame (negative just means a change to the left)
+vx = gradient(centroidsF2(:,1));
+% get the change in y-coordinate per frame
+vy = gradient(centroidsF2(:,2));
+% velocity 
+vel = ((sqrt(vx.^2 + vy.^2)));
+clear vx vy;
+vel = fillmissing(vel,'linear');
+vel = movmedian(vel,80/2);
+% from pixels per frame to mm per second
+vel = vel * 80 * .51 / 1000;
+
+
+% Create skeleton: each row will contain a lag and a speed to create a scatterplot
+strides = nan(1,1);
+for k = 1 : min( [length(maxpkx{1,2}); length(maxpkx{1,1})]  )-1
+    % Forepaw vector of normalized distances from TTI from one max to the next
+    v1 = paws(maxpkx{1,1}(k,1) : maxpkx{1,1}(k+1,1) ,1);
+    % Hind paw vector of normalized distances from TTI from one max to the next
+%     v2 = paws(maxpkx{1,2}(k,1) : maxpkx{1,2}(k+1,1) ,2);
+    v2 = paws(maxpkx{1,1}(k,1) : maxpkx{1,1}(k+1,1) ,2);
+
+    % Use 'xcorr' function to get the lag for each iteration (stride)
+    [c, lags]  = xcorr(v1, v2);
+    % Normalize
+    c = abs(c)/max(c);
+    % Get the max value and its index:
+    [~,i] = max(c);
+    t = lags(i);
     
-% maxpkx{1,1}(index)
-% [r, lags] = xcorr(  maxpkx{1,1}(1:10,1), maxpkx{1,2}(1:10,1)  )
+    
+    
+    
+    
+    % Add lag and speed to a new row in 'strides'
+    strides(k,1) = t;
+%     strides(k,2) = speed;
+end
 
-% Get the cross correlation of the y-values from maxpk(i) to maxpk(i+1)
-v1 = paws(   maxpkx{1,1}(1,1) : maxpkx{1,1}(2,1)   ,1);
-v2 = paws(   maxpkx{1,2}(1,1) : maxpkx{1,2}(2,1)   ,2);
-[r, lags]  = xcorr(v1, v2)
+% Plot current stride
+figure(3)
+plot(v1); hold on; plot(v2)
 
+
+% Plot stride with 3 frame shift
+figure(1)
+plot((1:length(v1))+3, v1); hold on; plot(v2)
 
 
 
 %%
 figure(2)
-findpeaks(paws(:,1),'MinPeakDistance', 11)
+findpeaks(paws(1:1000,1),'MinPeakDistance', 11)
 hold on
-findpeaks(paws(:,2),'MinPeakDistance', 11)
+findpeaks(paws(1:1000,2),'MinPeakDistance', 11)
 
 
