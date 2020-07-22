53940f3c6ff321401e1ae30d154f45613abc535b
johnduva <johnduva83@gmail.com>
2020-07-22 15:46:26 -0400

    Jul 22 1546

--- John Analysis/July/clusteringSpeedLagData.m
+++ John Analysis/July/clusteringSpeedLagData.m
@@ -40,7 +40,6 @@ load('/Users/johnduva/iCloud Drive (Archive) - 1/Desktop/mkislin Files/corrected
 load('/Users/johnduva/iCloud Drive (Archive) - 1/Desktop/mkislin Files/allTracks.mat');
 load('/Users/johnduva/iCloud Drive (Archive) - 1/Desktop/mkislin Files/keepersOnly.mat');
 load('/Users/johnduva/iCloud Drive (Archive) - 1/Desktop/mkislin Files/filenames.mat') % files_by_day.mat
-
 load('/Users/johnduva/Git/StrideAnalysis/John Analysis/Mat Files/phenosAll.mat')  % phenos
 load('/Users/johnduva/Git/StrideAnalysis/John Analysis/Mat Files/ASD_all.mat') 
 load('/Users/johnduva/Git/StrideAnalysis/John Analysis/Mat Files/Cntnap2_all.mat')
@@ -48,7 +47,7 @@ load('/Users/johnduva/Git/StrideAnalysis/John Analysis/Mat Files/Cntnap2_all.mat
 % Which gait/cluster do we want to aggregate movies of? 
 cluster = 3;
 
-% For each bout in permaList, identify if in cluster - then create movie if it is
+% For each bout in permaList, identify if in current cluster; if so, create movie
 for bout = 1:length(permaList)
     % if that bout is a 0 lag bout
     if idx(bout)==cluster
@@ -61,7 +60,6 @@ for bout = 1:length(permaList)
         pORa = ASD_all;
         % Het(1), Homo(2), or Neg(3)
         phe = 1; day = 1;
-        
         
         allPaws = permute( correctedTens5{pORa{phe}(1,an),day}([5,6,9,10], : , :), [2 1 3]);
 
@@ -78,17 +76,15 @@ for bout = 1:length(permaList)
         vid = sprintf('%04d',cell2mat(files_by_day(pORa{phe}(1,an),day)));
         % Load rotVal from proper file
         load(['/Users/johnduva/Desktop/Stride Figures/ASDvids/OFT-', vid, '-00_box_aligned_info.mat'], 'mouseInfo')
-
-        % Change coordinates to real space
+        
+        %%%%%%%%%%%%%%% Change coordinates to real space %%%%%%%%%%%%%%%%%%
         allPaws2 = permute(allPaws,[2 1 3]);
         tempr = deg2rad(mouseInfo.rotVal);
         tempr = tempr(keepersOnly{pORa{phe}(1,an),day});
         midJoints = double(allPaws2) - 200;
-
         jx = double(squeeze(midJoints(:,1,:)));
         jy = double(squeeze(midJoints(:,2,:)));
-        ff = zeros(point_size(allPaws2));
-
+        ff = zeros(size(allPaws2));
         for i = 1:length(jx)
         [jp2j(:,i), jp2i(:,i)] = cart2pol( jx(:,i)', jy(:,i)' );
         tjp(:,i) = jp2j(:,i) + repmat(tempr(i),[4 1]);
@@ -96,13 +92,43 @@ for bout = 1:length(permaList)
         ogc1(:,i) = jp3j(:,i) + centroidsF2(i,1);
         ogc2(:,i) = jp3i(:,i) + centroidsF2(i,2);
         end
-
         ff(:,1,:) = ogc1; 
         ff(:,2,:) = ogc2;
         clearvars jp2i jp2j jp3j jp3i jx jy midJoints ogc1 ogc2 tempt tempr tempCents tjp;
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        %% Show video of four paws in real space
+        boutNum = sprintf('%04d',bout);
+        v = VideoWriter(['tscHet_Gait3_', boutNum, '.mp4'], 'MPEG-4');
+        v.FrameRate = 10;
+        open(v);
+        % slot = 1:velTscHet_HIGH(:,1);
+        % Generate a set of frames, get the frame from the figure, and then write each frame to the file.
+        for k = permaList(bout,3) : permaList(bout,4)
+            h1 = scatter(ff(1,1,k), ff(1,2,k), 'r'); % this is 5
+            hold on;
+            h2 = scatter(ff(2,1,k), ff(2,2,k), 'b'); % this is 6
+            hold on;
+            h3 = scatter(ff(3,1,k), ff(3,2,k), 'b'); % this is 9
+            hold on;
+            h4 = scatter(ff(4,1,k), ff(4,2,k), 'r'); % this is 10
 
-        
-        
+            % legend('Hindpaws','Forepaws', 'Location','NorthWest')
+            title('Cntnap Het')
+            xlim([0 1000])
+            ylim([0 1200])
+            frame = getframe(gcf);
+            writeVideo(v,frame);
+
+            hold off;
+            delete(findall(gcf,'Tag','stream'));
+            label1 = 'Frame = %d';
+            str = sprintf(label1, k);
+            annotation('textbox',[.75 .8 .1 .1],'string',str, 'Tag', 'stream')
+            drawnow;
+        end
+
+        close(v);
         
     end
 end
